543b710a0113dafeaea50e0214840f24b5460645
dev <dev@mostafas-MacBook-Pro.fritz.box>
2022-04-15 20:07:40 +0200

    install supabase, bootstrap, update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { supabase } from './config/supabase.config';
+
+type tPost = {
+  id?: any,
+  title: string,
+   content: string
+}
+
+const defaultPostData: tPost[] = [{
+  title: "",
+  content: ""
+
+}]
 
 function App() {
+
+  const [posts, setPosts] = useState<tPost[]>(defaultPostData)
+  const [post, setPost] = useState<tPost>({ title: "", content: ""})
+  const {title, content} = post;
+
+
+  const fetchPosts = async () => {
+    const { data } = await supabase.from('testTB').select()
+    data ? setPosts(data) : null
+  }
+
+  const createPost = async () => {
+     await supabase.from('testTB')
+    .insert([
+      {title, content}
+    ]).single()
+    setPost({ title: "", content: ""})
+    fetchPosts()
+    
+  } 
+
+  useEffect(() => {
+fetchPosts()
+  })
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <input type="text" value={title} placeholder="Title"
+       onChange={(e)=> setPost({...post, title: e.target.value})}
+      />
+      <input type="text" value={content} placeholder="Content"
+       onChange={(e)=> setPost({...post, content: e.target.value})}
+      />
+
+      <button onClick={createPost}>Create Post</button>
+
+      {
+        posts.map( (post: tPost, index) => (
+           <div key={post.id}>
+             <span>{post.title}</span>:<span>{post.content}</span>
+           </div>
+        ))
+      }
     </div>
   );
 }
